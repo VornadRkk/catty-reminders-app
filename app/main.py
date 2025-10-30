2038b7ad78a16c6a2c615864ce9c6ad290de4086
VornadRkk <epra11111@mail.ru>
2025-10-30 15:00:16 +0000

    Tesst2 webhook on labа1

--- app/main.py
+++ app/main.py
@@ -66,7 +66,7 @@ def custom_openapi():
     """
 
   openapi_schema = get_openapi(
-    title="CattSASHAy: The Reminders App",
+    title="CattSASHAAy: The Reminders App",
     version="1.0.0",
     description=description,
     routes=app.routes,
